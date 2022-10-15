efedea123c648cac9033d02abf0514ad7067e21b
AlinaD <alina.dorosh@gmail.com>
2022-10-15 21:08:12 +0200

    Ex8 from Algoritms3 DONE

--- TEAM_D/assets/js/algorithms.js
+++ TEAM_D/assets/js/algorithms.js
@@ -10,6 +10,8 @@ let ex5Alg2 = function (stringManager) {
     capitalize: false,
     replaceSpaces: false,
   };
+  let splitedMsg = msg.split(" ");
+  let msgToLetterArray = [...msg];
 
   stringManager = function (msg, answers) {
     let option1 = prompt("Replace chars by signs? y/n");
@@ -27,6 +29,14 @@ let ex5Alg2 = function (stringManager) {
         "%c" + replacedS,
         "font-weight:bold; font-size: 12px;color: red"
       );
+      let counter = 0;
+      for (let i = 0; i < msgToLetterArray.length; i++) {
+        let finalText = [...replacedS];
+        if (msgToLetterArray[i] !== finalText[i]) {
+          counter++;
+        }
+      }
+      console.log(`${counter} characters changed`);
     }
 
     let option2 = prompt("Capitalize? y/n");
@@ -35,8 +45,6 @@ let ex5Alg2 = function (stringManager) {
       answers.capitalize = true;
     }
     if (answers.capitalize == true) {
-      let splitedMsg = msg.split(" ");
-
       for (let index = 0; index < splitedMsg.length; index++) {
         splitedMsg[index] =
           splitedMsg[index].charAt(0).toUpperCase() +
@@ -53,7 +61,6 @@ let ex5Alg2 = function (stringManager) {
       answers.replaceSpaces = true;
     }
     if (answers.replaceSpaces == true) {
-      let splitedMsg = msg.split(" ");
       for (let index = 0; index < splitedMsg.length; index++) {
         if (splitedMsg[index].endsWith(",")) {
           splitedMsg[index] = splitedMsg[index].slice(0, -1);
@@ -126,8 +133,98 @@ let ex5Alg3 = function () {
   let carRegistration = numbers.join("") + letters.join("");
 
   console.log(
-    `%c Your new car registration number is`, `font-weight:bold; font-size: 20px;color: #288BA8 `);
-  console.log(`%c ${carRegistration}`,`font-weight:bold; font-size: 20px;color: #F06000 `);
+    `%c Your new car registration number is`,
+    `font-weight:bold; font-size: 20px;color: #288BA8 `
+  );
+  console.log(
+    `%c ${carRegistration}`,
+    `font-weight:bold; font-size: 20px;color: #F06000 `
+  );
 };
 
 //ex5Alg3();
+
+//Ejercicio 8 PDF-Algoritmos3 /Alina
+function ex8Alg3(){
+  const cardDeck = ["clubs", "hearts", "spades", "diamonds"];
+  const cards = ["ACE", "KING", "QUEEN", "JACK", 10, 9, 8, 7, 6, 5, 4, 3, 2];
+  const startBalance = 500;
+  let initialBalance = startBalance;
+  let actualBalance;
+  let userName = prompt("Please enter your name").toUpperCase();
+  let game = prompt(`HELLO ${userName}, DO YOU WANT TO PLAY A GAME? y/n`);
+  let bet;
+  if (game == "y") {
+    letsPlay();
+  }
+
+  function letsPlay() {
+    bet = prompt("Insert your bet");
+    if (bet > initialBalance) {
+      alert("The bet is above your ballance, please try again!");
+    } else if (isNaN(bet)) {
+      alert("You should enter only numeric values,please try again!");
+    } else {
+      let computersCardDeck =
+        cardDeck[Math.floor(Math.random() * cardDeck.length)];
+
+      let computersCardNumber = cards[Math.floor(Math.random() * cards.length)];
+      console.log(
+        `Virtual gamer get ${computersCardDeck}${computersCardNumber}`
+      );
+
+      let usersCardDeck = cardDeck[Math.floor(Math.random() * cardDeck.length)];
+
+      let usersCardNumber = cards[Math.floor(Math.random() * cards.length)];
+
+      console.log(`${userName} get ${usersCardDeck}${usersCardNumber}`);
+
+      if (usersCardNumber == "JACK") {
+        usersCardNumber = 11;
+      } else if (usersCardNumber == "QUEEN") {
+        usersCardNumber = 12;
+      } else if (usersCardNumber == "KING") {
+        usersCardNumber = 13;
+      } else if (usersCardNumber == "ACE") {
+        usersCardNumber = 14;
+      }
+      if (computersCardNumber == "JACK") {
+        computersCardNumber = 11;
+      } else if (computersCardNumber == "QUEEN") {
+        computersCardNumber = 12;
+      } else if (computersCardNumber == "KING") {
+        computersCardNumber = 13;
+      } else if (computersCardNumber == "ACE") {
+        computersCardNumber = 14;
+      }
+      if (computersCardNumber < usersCardNumber) {
+        actualBalance = initialBalance + parseInt(bet);
+        console.log(`${userName} WINS!!!!! Your balance now:${actualBalance}€`);
+      } else if (computersCardNumber > usersCardNumber) {
+        actualBalance = initialBalance - parseInt(bet);
+        console.log(
+          `You lose. Virtual gamer WINS!!!!! Your balance now:${actualBalance}€`
+        );
+      } else {
+        console.log("DRAW!");
+        actualBalance = initialBalance;
+      }
+    }
+    initialBalance = actualBalance;
+  }
+  if (actualBalance === 0) {
+    alert(`${userName} You have lost all your money. Your balance is 0`);
+  }
+  do {
+    let keepPlaying = prompt(`${userName} Do you want to keep playing? y/n`);
+    if (keepPlaying == "y") {
+      letsPlay();
+    } else {
+      let totalBenefit = actualBalance - startBalance;
+      alert(`Betting benefits: ${totalBenefit}€`);
+      alert(`Total balance:${actualBalance}€`);
+    }
+  } while (actualBalance > 0);
+};
+
+//ex8Alg3();
