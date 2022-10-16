9ccb06d8a51ed2242ab955f66337247cbaf0fa17
Cococod3 <greenoliverj@gmail.com>
2022-10-16 15:56:52 +0200

    Saved in function + refactores code. Exercises 6/2 and 2/3

--- js/algorithms.js
+++ js/algorithms.js
@@ -1,55 +1,59 @@
-// // ESTADÍSTICA DE TEXTO --------------------
+// ESTADÍSTICA DE TEXTO --------------------
+function textStats(wordCount) {
+    
+wordCount = prompt("Add some text");
+// WORD LENGTH --------------
+console.log(`Length: ${wordCount.length}`);
+// LENGTH WITHOUT SPACES ----------
+console.log(`Length without white-space: ${wordCount.split(" ").length}`);
+// UNIQUE WORDS ------------
+const countUnique = new Set(wordCount.split(" ")).size;
+console.log(`Unique words: ${countUnique}`);
+// PERCENT OF UNIQUE WORDS -------------09
+const percentUnique = Math.round((countUnique / wordCount.length) * 100);
+console.log(`Percent of unique words: ${percentUnique}%`);
+// SHORTEST WORD -------------
+function findShortestWord(wordCount) {
+    var words = wordCount.split(' ');
+    var shortest = words.reduce((shortestWord, currentWord) => {
+      return currentWord.length < shortestWord.length ? currentWord : shortestWord;
+    }, words[0]);
+    return shortest;
+  }
+  console.log("The shortest word is: ", findShortestWord(wordCount));
+//   LONGEST WORD-----
+function getLongestWord(wordCount) {
+  let words = wordCount.split(" ");
+  let maxLength = 0;
+  let longestWord = "";
 
-// let wordCount = prompt("Add some text");
-// // WORD LENGTH --------------
-// console.log(`Length: ${wordCount.length}`);
-// // LENGTH WITHOUT SPACES ----------
-// console.log(`Length without white-space: ${wordCount.split(" ").length}`);
-// // UNIQUE WORDS ------------
-// const countUnique = new Set(wordCount.split(" ")).size;
-// console.log(`Unique words: ${countUnique}`);
-// // PERCENT OF UNIQUE WORDS -------------09
-// const percentUnique = Math.round((countUnique / wordCount.length) * 100);
-// console.log(`Percent of unique words: ${percentUnique}%`);
-// // SHORTEST WORD -------------
-// function findShortestWord(wordCount) {
-//     var words = wordCount.split(' ');
-//     var shortest = words.reduce((shortestWord, currentWord) => {
-//       return currentWord.length < shortestWord.length ? currentWord : shortestWord;
-//     }, words[0]);
-//     return shortest;
-//   }
-//   console.log(findShortestWord(wordCount));
-// //   LONGEST WORD-----
-// function getLongestWord(wordCount) {
-//   let words = wordCount.split(" ");
-//   let maxLength = 0;
-//   let longestWord = "";
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+      longestWord = words[i];
+    }
+  }
+  console.log("The longest word is: ", longestWord);
+}
 
-//   for (let i = 0; i < words.length; i++) {
-//     if (words[i].length > maxLength) {
-//       maxLength = words[i].length;
-//       longestWord = words[i];
-//     }
-//   }
-//   console.log(longestWord);
-// }
+getLongestWord(wordCount);
+// AVARAGE UNIQUE WORD COUNT-------
 
-// getLongestWord(wordCount);
-// // AVARAGE WORD COUNT-------
-
-// const averageWordLength = wordCount => {
-//    if(!wordCount.includes(' ')){
-//       return wordCount.length;
-//    };
-//    const { length: strLen } = wordCount;
-//    const { length: numWords } = wordCount.split(' ');
-//    const average = (strLen - numWords + 1) / numWords;
-//    return average.toFixed(2);
-// };
-// console.log(averageWordLength(wordCount));
-
+const averageWordLength = wordCount => {
+   if(!wordCount.includes(' ')){
+      return wordCount.length;
+   };
+   const { length: strLen } = wordCount;
+   const { length: numWords } = wordCount.split(' ');
+   const average = (strLen - numWords + 1) / numWords;
+   return average.toFixed(2);
+};
+console.log("This is the avarage word length: ", Math.round(averageWordLength(wordCount)));
+}
+textStats ("This is the text statistics function")
 // EX. 2/3 HORA DEL SISTEMA + NOMBRE USUARIO:
+function dayGreetings(greetings) {
+    
 let userName = prompt("Add user name")
 const fechaDehoy = new Date();
 let hour = fechaDehoy.getHours();
@@ -61,12 +65,13 @@ if (hour < 12 ) welcomeText = welcomeTypes[0];
 else if (hour < 18) welcomeText = welcomeTypes[1];
 else welcomeText = welcomeTypes [2];
 
-function greeting(welcomeText, userName) {
+function greeting(welcomeText) {
 alert(`${welcomeText}`)
 
 }
 
 greeting(`${welcomeText} ${userName}`);
-
+}
+dayGreetings("This is the greetings function")
 
 
