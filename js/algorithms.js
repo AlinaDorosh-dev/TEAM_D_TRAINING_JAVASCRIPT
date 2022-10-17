baa1b38dc039435e85bcce78ba420f8bc1d58a0f
Fran Alonso <franciscojalonso91@gmail.com>
2022-10-17 13:23:03 +0200

    Rehecho ejercicio 4

--- js/algorithms.js
+++ js/algorithms.js
@@ -1,40 +1,42 @@
 //Exercise 4 Algorithms2
-// let promedio = (temperaturas) => {
-//   let tempMaxima = Math.max(...temperaturas);
-//   let tempMinima = Math.min(...temperaturas);
-
-//   let promedio = (tempMaxima + tempMinima) / 2;
-
-//   console.log("La temperatura promedio en Sevilla fué de: " + promedio);
-// };
-
-// promedio([31.7, 30.7, 32.5, 29.4, 31.6, 28.5, 31.7]);
-
+function mediaTemperatura(temperatura) {
+  var i = 0,
+    summ = 0,
+    ArrayLen = temperatura.length;
+  while (i < ArrayLen) {
+    summ = summ + temperatura[i++];
+  }
+  return summ / ArrayLen;
+}
+var temperature = [31.7, 30.7, 32.5, 29.4, 31.6, 28.5, 31.7];
+var a = mediaTemperatura(temperature);
+console.log(Math.floor(a));
 //Exercise 7 Algorithms2
-let min = 0;
-let max = 10;
-let numberRandom = Math.floor(Math.random() * (max - min + 1)) + min;
-console.log(numberRandom);
-let guessNumber;
-let centinela = false;
-let tries = 3;
 
-while (!centinela && tries >= 1) {
-  if (tries == 3) {
-    guessNumber = parseInt(prompt(`Insert a number, tries ${tries}`));
-  } else {
-    guessNumber = parseInt(
-      prompt(`Try again, you have ${tries} tries`)
-    );
-  }
+// let min = 0;
+// let numberRandom = Math.floor(Math.random() * (max - min + 1)) + min;
+// let max = 10;
+// console.log(numberRandom);
+// let guessNumber;
+// let centinela = false;
+// let tries = 3;
 
-  if (numberRandom == guessNumber) {
-    centinela = true;
-  }
-  tries--;
-}
-if (centinela) {
-  alert("You win!");
-} else {
-  alert("Sorry, Good luck in love!");
-}
+// while (!centinela && tries >= 1) {
+//   if (tries == 3) {
+//     guessNumber = parseInt(prompt(`Insert a number, tries ${tries}`));
+//   } else {
+//     guessNumber = parseInt(
+//       prompt(`Try again, you have ${tries} tries`)
+//     );
+//   }
+
+//   if (numberRandom == guessNumber) {
+//     centinela = true;
+//   }
+//   tries--;
+// }
+// if (centinela) {
+//   alert("You win!");
+// } else {
+//   alert("Sorry, Good luck in love!");
+// }
